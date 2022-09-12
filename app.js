0bdf69ceff405165101b2beea0826300a0932406
Narendra Mali <68180334+narendra-mali@users.noreply.github.com>
2022-09-12 15:50:13 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
 const express = require('express')
-const app = express();
-const studentRoute = require('./api/routes/student')
-const facultyRoute = require('./api/routes/faculty')
-const mongoose = require('mongoose'); 
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost/AlienDBex'
 
-mongoose.connect('mongodb+srv://narendramali:123@sbs.bpykitf.mongodb.net/?retryWrites=true&w=majority')
+const app = express()
 
-mongoose.connection.on('error',err=>{
-    console.log("connection failed");
-});
-mongoose.connection.on('connected',connected=>{
-    console.log("connectd with database");
-});
+mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-app.use('/student',studentRoute)
-app.use('/faculty',facultyRoute)
-
-app.use((req,res,next)=>{
-    res.status(404).json({
-        error : 'bad request'
-    })
+con.on('open', () => {
+    console.log('connected successfully...')
 })
 
-module.exports = app;
+app.use(express.json())
 
+const alienRouter = require('./routes/aliens')
+app.use('/aliens',alienRouter)
 
+app.listen(3000, () => {
+    console.log('Server started')
+})
